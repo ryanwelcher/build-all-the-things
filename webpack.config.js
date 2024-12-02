b198fa0a0f2d77b608e10c4c39e0d4077e6fafec
Ryan Welcher <me@ryanwelcher.com>
2024-12-02 15:57:50 -0500

    Basic setup

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// Import the original config from the @wordpress/scripts package.
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+
+// Add any a new entry point by extending the webpack config.
+module.exports = {
+	...defaultConfig,
+	entry: {
+		...defaultConfig.entry(),
+		variations: "./js/variations.js",
+		slots: "./js/slots.js",
+	},
+};
